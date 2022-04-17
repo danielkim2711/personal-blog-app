2f2fdaa8a322e62ca75ca6db4d785227ae2d3801
Daniel Kim <danielkim2711@gmail.com>
2022-04-17 21:40:07 +1200

    Use express async handler to use async await

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,37 +1,39 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    Get posts
 // @route   GET /api/posts
 // @access  Public
-const getPosts = async (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Posts' });
-};
+});
 
 // @desc    Get post
 // @route   GET /api/posts/:id
 // @access  Public
-const getPost = async (req, res) => {
+const getPost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get Post ${req.params.id}` });
-};
+});
 
 // @desc    Create post
 // @route   POST /api/posts
 // @access  Private
-const createPost = async (req, res) => {
+const createPost = asyncHandler(async (req, res) => {
   res.status(201).json({ message: 'Create Post' });
-};
+});
 
 // @desc    Update post
 // @route   PUT /api/posts/:id
 // @access  Private
-const updatePost = async (req, res) => {
+const updatePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Post ${req.params.id}` });
-};
+});
 
 // @desc    Delete post
 // @route   DELETE /api/posts/:id
 // @access  Private
-const deletePost = async (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Post ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPosts,
