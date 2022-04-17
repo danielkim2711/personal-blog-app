b1d976de0182d03bb6dfdadb7863e5646d0b81d6
Daniel Kim <danielkim2711@gmail.com>
2022-04-17 21:38:45 +1200

    Overwrite existing express error handler

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8000;
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.use('/api/posts', require('./routes/postRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on PORT ${port}`));
