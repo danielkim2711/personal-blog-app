94002bdeb9880f1ba1c425606c92a3ac274827b3
Daniel Kim <danielkim2711@gmail.com>
2022-05-15 00:22:38 +1200

    Deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
@@ -14,6 +15,22 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  );
+} else {
+  app.get('/', (req, res) =>
+    res
+      .status(200)
+      .json({ message: "Welcome to Daniel's Personal Blog App API" })
+  );
+}
 
 app.use(errorHandler);
 
