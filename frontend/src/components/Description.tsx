5488c7432ac5543af1f8f0dd9c3b08dabf3dc69a
Daniel Kim <danielkim2711@gmail.com>
2022-07-23 21:22:26 +1200

    Change description

--- frontend/src/components/Description.tsx
+++ frontend/src/components/Description.tsx
@@ -7,7 +7,7 @@ const Description = () => {
         WHO AM I?
       </h2>
       <p className='mt-2 2xl:text-2xl'>
-        CS student at Unitec, future web dev 💪
+        CS graduate at Unitec, future web dev 💪
       </p>
       <ul className='flex'>
         <li className='mr-2'>
